20bdc6b8b871a15c8d86db9651b4a3086c53ad9f
Lavesh Bhagwani <laveshthecoder@gmail.com>
2026-01-11 03:18:02 +0530

    fix: resolve asset path and layout spacing issues

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Marquee() {
   return (
     <div className="w-full h-[50vh] bg-[#004D43]">
-        
+        <div className="text">
+            h1
+        </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../assets/images/logo.png'
+import Logo from '../assets/images/Logo.png'
 import {Menu , Phone} from 'lucide-react'
 
 function Navbar() {
