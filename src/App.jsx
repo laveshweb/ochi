2c9cff45d1f7799c8d189c936f253a3703e2a15c
Lavesh Bhagwani <laveshthecoder@gmail.com>
2026-01-11 03:16:12 +0530

    fix: resolve asset path and layout spacing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Landingpage from './components/Landingpage'
+import Marquee from './components/Marquee'
 
 function App() {
   return (
     <div className='w-full h-screen bg-[#f1f1f1]  '>
       <Navbar/>
       <Landingpage/>  
+      <Marquee/>
       </div>
   )
 }
