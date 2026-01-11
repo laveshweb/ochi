18b7b5a00859e3f01ded3821a07c54586925a36b
Lavesh Bhagwani <laveshthecoder@gmail.com>
2026-01-11 15:06:15 +0530

    Fix button text alignment and spacing

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -11,7 +11,7 @@ const Landingpage = () => {
                 <h1 className='uppercase font-sans font-bold  leading-none tracking-tighter relative top-1  lg:h-[100px] scale-y-[1.30] max-[400px]:-translate-y-[3px]'>We Create</h1>
                 <div className='relative left-0 sm:left-2 flex'>
                   <img src={LandingPage} alt="" className=' 
-              w-[80px]  max-[440px]:h-[48px] sm:w-[110px] sm:h-auto  md:w-[130px] lg:w-[160px]  relative top-[2px] sm:top-2 lg:top-0 lg:mt-3 p-1  rounded-xl ' />
+              w-[80px]  max-[400px]:h-[48px] max-[440px]:h-[55px]  sm:w-[110px] sm:h-auto  md:w-[130px] lg:w-[160px]  relative top-[1px] min-[400px]:top-1 lg:top-0 lg:mt-3 p-1  rounded-xl ' />
                 <h1 className='uppercase font-sans font-bold  -leading-[5vw] tracking-tighter lg:h-[100px] scale-y-[1.30]'>Eye-Opening</h1>
                 </div>
                 <h1 className='uppercase font-sans font-bold -leading-[2vw] tracking-tighter relative -top-4   min-[765px]:top-0 lg:h-[100px]  scale-y-[1.30] max-[400px]:mt-[5px]'>Presentations</h1>
@@ -20,11 +20,11 @@ const Landingpage = () => {
             <div className="border-t-2 border-zinc-800 mt-28   flex justify-between  items-center flex flex-col min-[800px]:flex-row ">
               {
                 ["for public and private companies", "from the first pitch to the pitch IPO"].map((item , index)=>
-                  <p className='text-md font-light tracking-tight leading-none px-6 font-normal p-4 sm:p-5 '>{item}</p>
+                  <p className='text-md font-light tracking-tight leading-none px-6 font-normal p-4 sm:p-5'>{item}</p>
               )
               }
               <div className="Startup flex  gap-3 p-4 sm:p-5">
-                <div className='px-3 py-2 border-1 border-zinc-700 font-normal  text-sm rounded-full mr-5 mt-[4px] '>Start The Project</div>
+                <div className='px-3  border-1 border-zinc-700 font-normal   leading-none  translate text-sm rounded-full mr-5 flex justify-center items-center '>Start The Project</div>
                 <div className=' rounded-full  mt-1 relative right-6  '><FaArrowRight className='w-10 h-10 border-[1.5px] rounded-full p-[10px] rotate-[-45deg]' /></div>
               </div>
             </div>
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,10 +1,15 @@
+import { animate, motion } from 'framer-motion'
 import React from 'react'
+
 
 function Marquee() {
   return (
-    <div className="w-full h-[50vh] bg-[#004D43]">
-        <div className="text">
-            h1
+    <div className="w-full py-20 rounded-tl-3xl  rounded-tr-3xl bg-[#004D43] ">
+        <div className="text border-t-2 border-b-2  border-zinc-300 flex whitespace-nowrap  text-[40vw] sm:text-[34vw] md:text-[24vw] md:text-[16vw]  lg:text-[16vw] uppercase text-white leading-none 
+          font-['Founders_Grotesk_X-Cond_Bold'] font-sans font-bold gap-10 overflow-hidden -translate-y-1 ">
+            <motion.h1 initial={{x : 0}} animate={{x : "-100%"}} transition={{ease : "linear" , repeat : Infinity, duration : 5}}>We are Ochi</motion.h1>
+            <motion.h1 initial={{x : 0}} animate={{x : "-100%"}} transition={{ease : "linear" , repeat : Infinity, duration : 5}}>We are Ochi</motion.h1>
+            <motion.h1 initial={{x : 0}} animate={{x : "-100%"}} transition={{ease : "linear" , repeat : Infinity, duration : 5}}>We are Ochi</motion.h1>
         </div>
     </div>
   )
